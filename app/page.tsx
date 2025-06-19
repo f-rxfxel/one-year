ac8453b4c69bfd0b5846c6b7c2750bec82e060f2
Francisco Porfirio <r.porfirio010@gmail.com>
2025-06-19 00:31:13 -0300

    Adicionando configuração do ESLint para ignorar erros durante build, atualizando cores e animações dos ícones das estações, ajustes nas descrições e responsividade dos componentes, animação das letras T e E.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 
 import localFont from 'next/font/local'
 import T from '@/components/teamo/T';
+import E from '@/components/teamo/E';
 import Winter from '@/components/icons/winter';
 import Summer from '@/components/icons/summer';
 import Autumn from '@/components/icons/autumn';
@@ -15,60 +16,52 @@ const TheSeasons = localFont({
 })
 
 const seasons = {
-  winter: {
-    key: 'winter',
-    title: 'DOIS',
-    subtitle: 'INVERNOS',
-    description:
-      'O inverno é a estação mais fria do ano, caracterizada por temperaturas baixas, dias mais curtos e noites mais longas. É um período de introspecção e aconchego, onde a natureza se prepara para um novo ciclo.',
-    images: ['/images/winter 1.png', '/images/winter 2.png'],
-    icon: (color: string) => <Winter fill={color} />,
-    color: 'winter',
-  },
-  summer: {
-    key: 'summer',
-    title: 'DOIS',
-    subtitle: 'VEROES',
-    description:
-      'O verão é a estação mais quente do ano, com dias longos e ensolarados. É um período de energia, vitalidade e atividades ao ar livre, onde a natureza está em seu auge de exuberância.',
-    images: ['/images/summer 1.png', '/images/summer 2.png'],
-    icon: (color: string) => <Summer fill={color} />,
-    color: 'summer',
-  },
   autumn: {
     key: 'autumn',
     title: 'DOIS',
     subtitle: <span className="whitespace-nowrap">OU<T/><span className="-ml-1 md:-ml-4">ONOS</span></span>,
-    description:
-      'O outono é a estação da transição, onde as folhas mudam de cor e caem, preparando a natureza para o inverno. É um período de transformação e reflexão, com temperaturas amenas e paisagens douradas.',
     images: ['/images/autumn 1.png', '/images/autumn 2.png'],
-    icon: (color: string) => <Autumn fill={color} />,
+    icon: (color: string) => <Autumn fill={color} className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' />,
     color: 'autumn',
+  },
+  winter: {
+    key: 'winter',
+    title: 'DOIS',
+    subtitle: <span className="whitespace-nowrap"><span className="-mr-0 md:-mr-0">INV</span><E/><span>RNOS</span></span>,
+    images: ['/images/winter 1.png', '/images/winter 2.png'],
+    icon: (color: string) => <Winter fill={color} className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' />,
+    color: 'winter',
   },
   spring: {
     key: 'spring',
     title: 'DUAS',
     subtitle: 'PRIMAVERAS',
-    description:
-      'A primavera é a estação do renascimento, onde a natureza desperta do inverno. É um período de renovação, com flores desabrochando, temperaturas amenas e dias mais longos, anunciando a chegada do verão.',
     images: ['/images/spring 1.png', '/images/spring 2.png'],
-    icon: (color: string) => <Spring fill={color} />,
+    icon: (color: string) => <Spring fill={color} className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' />,
     color: 'spring',
+  },
+  summer: {
+    key: 'summer',
+    title: 'DOIS',
+    subtitle: 'VEROES',
+    images: ['/images/summer 1.png', '/images/summer 2.png'],
+    icon: (color: string) => <Summer fill={color} className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' />,
+    color: 'summer',
   },
 } as const;
 
 const seasonKeys = Object.keys(seasons) as (keyof typeof seasons)[];
 
 export default function Component() {
-  const [seasonKey, setSeasonKey] = useState<keyof typeof seasons>('winter');
+  const [seasonKey, setSeasonKey] = useState<keyof typeof seasons>('autumn');
   const [prevSeasonKey, setPrevSeasonKey] = useState<keyof typeof seasons | null>(null);
   const [isScrolling, setIsScrolling] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      const x = (event.clientX / window.innerWidth - 0.5) * 20;
-      const y = (event.clientY / window.innerHeight - 0.5) * 20;
+      const x = (event.clientX / window.innerWidth - 0.5) * 35;
+      const y = (event.clientY / window.innerHeight - 0.5) * 35;
       setMousePosition({ x, y });
     };
   
@@ -120,7 +113,7 @@ export default function Component() {
     src={imgSrc}
     alt=""
     fill
-    className={`absolute top-0 left-0 transition-opacity duration-500 ${
+    className={`absolute top-0 left-0 transition-opacity duration-500 opacity-50 ${
       visible ? 'opacity-100 z-10' : 'opacity-0 z-0'
     }`}
     style={{
@@ -148,16 +141,16 @@ export default function Component() {
       <main
         className={`md:m-12 m-6 size-full border border-${season.color}-highlight flex flex-col items-center justify-center`}
       >
-        <div className="flex items-center md:gap-12 gap-6">
+        <div className="flex items-center md:gap-18 gap-20 pb-12">
           <span className={`md:text-xl text-lg font-medium text-${season.color}-highlight`}>09</span>
-          <div className={`transition-colors duration-500 text-${season.color}-highlight`}>
+          <div className={`transition-colors duration-500 text-${season.color}-highlight relative`}>
             {season.icon(`currentColor`)}
           </div>
           <span className={`md:text-xl text-lg font-medium text-${season.color}-highlight`}>06</span>
         </div>
         <h1 className={`lg:text-9xl sm:text-7xl text-5xl text-${season.color}-highlight ${TheSeasons.className}`}>{season.title}</h1>
         <h1 className={`lg:text-9xl sm:text-7xl text-5xl mb-4 text-${season.color}-highlight ${TheSeasons.className}`}>{season.subtitle}</h1>
-        <p className={`md:max-w-2/3 px-8 text-md text-justify border-${season.color}-highlight text-${season.color}-highlight`}>{season.description}</p>
+        <p className={`md:max-w-2/3 px-8 text-md text-justify border-${season.color}-highlight text-${season.color}-highlight`}>Em raios solares sem aviso, as folhas cairão mesmo enquanto ainda houver perfume de flor; o frio se instalará em meio ao calor, como o rio que se congela e depois se torna refrescante. A flor caída retorna à terra para dar força a outra flor. Portanto, eu morreria feliz, porque tudo é real e tudo está certo. O que for, quando for, é o que será o que é.</p>
       </main>
     
 
